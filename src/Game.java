7020c87d5319c3d0bdd275e42e1cbb4f32dac347
CheolminConanShin <scm0226@gmail.com>
2017-06-09 13:26:52 +0900

    GOAL: working stage

--- src/Game.java
+++ src/Game.java
@@ -2,4 +2,97 @@
  * Created by SDS on 2017-06-09.
  */
 public class Game {
+    public int firstSecretNumber;
+    public int secondSecretNumber;
+    public int thirdSecretNumber;
+    public int[] secretNumber = new int[3];
+    public int strike = 0;
+    public int ball = 0;
+    private boolean[] checkedGuessNumber = new boolean[3];
+    private boolean[] checkedSecretNumber = new boolean[3];
+    private int[] guessNumber = new int[3];;
+
+
+    public void setSecretNumber(int first, int second, int third) {
+       secretNumber[0] = first;
+       secretNumber[1] = second;
+       secretNumber[2] = third;
+    }
+
+    static void main(String[] args) {
+
+    }
+
+    public boolean isSolved(int first, int second, int third) {
+        checkStrike(first, second, third);
+        return strike == 3;
+    }
+
+    public void checkStrike(int first, int second, int third) {
+        guessNumber[0] = first;
+        guessNumber[1] = second;
+        guessNumber[2] = third;
+        checkStrikeForIndex(0);
+        checkStrikeForIndex(1);
+        checkStrikeForIndex(2);
+    }
+
+    private void checkStrikeForIndex(int i) {
+        if(secretNumber[i] == guessNumber[i]) {
+            strike ++;
+            checkedGuessNumber[i] = true;
+            checkedSecretNumber[i] = true;
+        }
+    }
+
+    public void checkBall(int first, int second, int third) {
+        guessNumber[0] = first;
+        guessNumber[1] = second;
+        guessNumber[2] = third;
+
+
+        if(isNotStrike(0)) {
+            if (!checkedSecretNumber[1]) {
+                if (guessNumber[0] == secretNumber[1]) {
+                    increaseBallAndCheck(0, 1);
+                }
+            } else if (!checkedSecretNumber[2]) {
+                if (guessNumber[0] == secretNumber[2]) {
+                    increaseBallAndCheck(0, 2);
+                }
+            }
+        }
+        if(isNotStrike(1)) {
+            if (!checkedSecretNumber[0]) {
+                if (guessNumber[1] == secretNumber[0]) {
+                    increaseBallAndCheck(0, 1);
+                }
+            } else if (!checkedSecretNumber[2]) {
+                if (guessNumber[1] == secretNumber[2]) {
+                    increaseBallAndCheck(1, 2);
+                }
+            }
+        }
+        if(isNotStrike(2)) {
+            if (!checkedSecretNumber[0]) {
+                if (guessNumber[2] == secretNumber[0]) {
+                    increaseBallAndCheck(2, 0);
+                }
+            } else if (!checkedSecretNumber[1]) {
+                if (guessNumber[2] == secretNumber[1]) {
+                    increaseBallAndCheck(2, 1);
+                }
+            }
+        }
+    }
+
+    private void increaseBallAndCheck(int guessIndex, int secretIndex) {
+        ball++;
+        checkedSecretNumber[secretIndex] = true;
+        checkedGuessNumber[guessIndex] = true;
+    }
+
+    private boolean isNotStrike(int index) {
+        return !checkedGuessNumber[index];
+    }
 }
--- tests/GameTest.java
+++ tests/GameTest.java
@@ -1,11 +1,84 @@
+import org.junit.Before;
 import org.junit.Test;
+import org.omg.PortableServer.THREAD_POLICY_ID;
 
 import static org.junit.Assert.*;
 
 public class GameTest {
+
+    private Game game;
+    private int FIRST_SECRET_NUMBER = 1;
+    private int SECOND_SECRET_NUMBER = 2;
+    private int THIRD_SECRET_NUMBER = 3;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new Game();
+        game.setSecretNumber(FIRST_SECRET_NUMBER, SECOND_SECRET_NUMBER, THIRD_SECRET_NUMBER);
+    }
+
     @Test
     public void createGame() throws Exception {
-        Game game = new Game();
         assertNotNull(game);
+    }
+
+    @Test
+    public void should_generate_three_random_numbers() throws Exception {
+        assertEquals(1, game.secretNumber[0]);
+        assertEquals(2, game.secretNumber[1]);
+        assertEquals(3, game.secretNumber[2]);
+    }
+
+    @Test
+    public void should_return_true_if_secretNumber_and_guessNumber_are_equal() throws Exception {
+        int first = FIRST_SECRET_NUMBER;
+        int second = SECOND_SECRET_NUMBER;
+        int third = THIRD_SECRET_NUMBER;
+        assertTrue(game.isSolved(first, second, third));
+    }
+
+    @Test
+    public void should_return_false_if_secretNumber_and_guessNumber_are_not_equal() throws Exception {
+        assertFalse(game.isSolved(2,3, 4));
+    }
+
+    @Test
+    public void should_return_one_strike_if_one_number_is_equal_and_same_position() throws Exception {
+        int first = FIRST_SECRET_NUMBER;
+        int second = 4;
+        int third = 5;
+        game.checkStrike(first, second, third);
+        assertEquals(1, game.strike);
+    }
+
+    @Test
+    public void should_return_one_ball_if_one_number_is_equal_and_different_position() throws Exception {
+        int first = 7;
+        int second = FIRST_SECRET_NUMBER;
+        int third = 5;
+        game.checkBall(first, second, third);
+        assertEquals(1, game.ball);
+    }
+
+    @Test
+    public void should_return_one_strike_and_zero_ball_when_123_and_received_114() throws Exception {
+        int first = FIRST_SECRET_NUMBER;
+        int second = 1;
+        int third = 4;
+        game.checkStrike(first,second,third);
+        game.checkBall(first,second,third);
+        assertEquals(1, game.strike);
+        assertEquals(0, game.ball);
+    }
+
+    @Test
+    public void should_return_three_strikes_when_123_equals_123() throws Exception {
+        int first = FIRST_SECRET_NUMBER;
+        int second = SECOND_SECRET_NUMBER;
+        int third = THIRD_SECRET_NUMBER;
+        game.checkStrike(first,second,third);
+        game.checkBall(first,second,third);
+        assertEquals(3, game.strike);
+        assertEquals(0, game.ball);
     }
 }
